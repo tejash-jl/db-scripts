f29a73de8b049557b89c03b877b37adc5e24056a
Tejash JL <tejash.jl1020@gmail.com>
2025-03-18 22:52:16 +0530

    fix params usage

--- db_scripts/mosip_esignet/db.sql
+++ db_scripts/mosip_esignet/db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE :'db'
+CREATE DATABASE :db
 	ENCODING = 'UTF8' 
 	LC_COLLATE = 'en_US.UTF-8' 
 	LC_CTYPE = 'en_US.UTF-8' 
@@ -6,11 +6,11 @@ CREATE DATABASE :'db'
 	OWNER = postgres
 	TEMPLATE  = template0;
 
-COMMENT ON DATABASE :'db' IS 'e-Signet related data is stored in this database';
+COMMENT ON DATABASE :db IS 'e-Signet related data is stored in this database';
 
-\c :'db' postgres
+\c :db postgres
 
 DROP SCHEMA IF EXISTS esignet CASCADE;
 CREATE SCHEMA esignet;
 ALTER SCHEMA esignet OWNER TO postgres;
-ALTER DATABASE :'db' SET search_path TO esignet,pg_catalog,public;
+ALTER DATABASE :db SET search_path TO esignet,pg_catalog,public;
