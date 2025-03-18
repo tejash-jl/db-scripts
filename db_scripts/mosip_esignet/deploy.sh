e88ecf9d79cfe37ba838f7cf28fcefac938e6cce
Tejash JL <tejash.jl1020@gmail.com>
2025-03-18 22:24:13 +0530

    Update deploy.sh

--- db_scripts/mosip_esignet/deploy.sh
+++ db_scripts/mosip_esignet/deploy.sh
@@ -1,5 +1,31 @@
 
 ## Properties file
 set -e
+properties_file="$1"
+echo `date "+%m/%d/%Y %H:%M:%S"` ": $properties_file"
+if [ -f "$properties_file" ]
+then
+     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file \"$properties_file\" found."
+    while IFS='=' read -r key value
+    do
+        key=$(echo $key | tr '.' '_')
+         eval ${key}=\${value}
+    done < "$properties_file"
+else
+     echo `date "+%m/%d/%Y %H:%M:%S"` ": Property file not found, Pass property file name as argument."
+fi
 
 echo "$DB_NAME"
+## Create DB
+PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f db.sql $DB_NAME
+##PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f ddl.sql 
+
+## Grants
+##PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -f grants.sql 
+
+## Populate tables
+##if [ ${DML_FLAG} == 1 ]
+##then
+  ##  echo `date "+%m/%d/%Y %H:%M:%S"` ": Deploying DML for ${MOSIP_DB_NAME} database" 
+    ##PGPASSWORD=$SU_USER_PWD psql -v ON_ERROR_STOP=1 --username=$SU_USER --host=$DB_SERVERIP --port=$DB_PORT --dbname=$DEFAULT_DB_NAME -a -b -f dml.sql 
+##fi
