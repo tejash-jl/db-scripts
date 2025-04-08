51119f0d79eace3ac2e96343f14af036c71fdc8b
Tejash JL <tejash.jl1020@gmail.com>
2025-04-08 13:59:16 +0530

    Update db.sql

--- db_scripts/mosip_certify/db.sql
+++ db_scripts/mosip_certify/db.sql
@@ -3,20 +3,10 @@
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 -- -------------------------------------------------------------------------------------------------
 
-CREATE DATABASE :db
-	ENCODING = 'UTF8' 
-	LC_COLLATE = 'en_US.UTF-8' 
-	LC_CTYPE = 'en_US.UTF-8' 
-	TABLESPACE = pg_default 
-	OWNER = postgres
-	TEMPLATE  = template0;
-
-COMMENT ON DATABASE :db IS 'Certify related data is stored in this database';
-
 \c :db postgres
 
-DROP SCHEMA IF EXISTS certify CASCADE;
+
 CREATE SCHEMA certify;
-ALTER SCHEMA certify OWNER TO postgres;
+
 ALTER DATABASE :db SET search_path TO certify,pg_catalog,public;
 
