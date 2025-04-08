7b06fa18f5f352fe8b1f33945ea157be8d225a5f
Tejash JL <tejash.jl1020@gmail.com>
2025-04-08 13:56:50 +0530

    Update db.sql

--- db_scripts/mosip_esignet/db.sql
+++ db_scripts/mosip_esignet/db.sql
@@ -1,12 +1,3 @@
-CREATE DATABASE :db
-	ENCODING = 'UTF8' 
-	LC_COLLATE = 'en_US.UTF-8' 
-	LC_CTYPE = 'en_US.UTF-8' 
-	TABLESPACE = pg_default 
-	OWNER = postgres
-	TEMPLATE  = template0;
-
-COMMENT ON DATABASE :db IS 'e-Signet related data is stored in this database';
 
 \c :db postgres
 
