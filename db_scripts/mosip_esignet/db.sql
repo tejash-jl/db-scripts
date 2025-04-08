0dc8a2067c64cec27669b756478441bed66702a2
Tejash JL <tejash.jl1020@gmail.com>
2025-04-08 13:57:50 +0530

    Update db.sql

--- db_scripts/mosip_esignet/db.sql
+++ db_scripts/mosip_esignet/db.sql
@@ -1,7 +1,6 @@
 
 \c :db postgres
 
-DROP SCHEMA IF EXISTS esignet CASCADE;
 CREATE SCHEMA esignet;
-ALTER SCHEMA esignet OWNER TO postgres;
+
 ALTER DATABASE :db SET search_path TO esignet,pg_catalog,public;
