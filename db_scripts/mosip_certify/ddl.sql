2630ab53c120f956bd0e639007f6a9a32280ac5b
Tejash JL <tejash.jl1020@gmail.com>
2025-06-11 14:05:48 +0530

    update ddl scripts

--- db_scripts/mosip_certify/ddl.sql
+++ db_scripts/mosip_certify/ddl.sql
@@ -1,12 +1,12 @@
 \c :db
 
-\ir ddl/certify-key_alias.sql
-\ir ddl/certify-key_policy_def.sql
-\ir ddl/certify-key_store.sql
-\ir ddl/certify-ca_cert_store.sql
-\ir ddl/certify-rendering_template.sql
-\ir ddl/certify-credential_template.sql
-\ir ddl/certify-ledger_issuance_table.sql
+\ir ddl/certify-ca_cert_store.sql 
+\ir ddl/certify-credential_status_transaction.sql 
+\ir ddl/certify-credential_template.sql 
+\ir ddl/certify-key_alias.sql 
+\ir ddl/certify-key_policy_def.sql 
+\ir ddl/certify-key_store.sql 
+\ir ddl/certify-ledger.sql 
+\ir ddl/certify-rendering_template.sql 
+\ir ddl/certify-status_list_available_indices.sql 
 \ir ddl/certify-status_list_credential.sql
-
-
