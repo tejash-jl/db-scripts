e112c147f7a453680172153d3d728f9d20dad4ab
Aparnadeloitte <aparnr@deloitte.com>
2025-10-23 17:10:21 +0530

    rm certify-credential_template.sql

--- db_scripts/mosip_certify/ddl.sql
+++ db_scripts/mosip_certify/ddl.sql
@@ -3,8 +3,7 @@
 \ir ddl/certify-ca_cert_store.sql 
 \ir ddl/certify-ledger.sql 
 \ir ddl/certify-status_list_credential.sql
-\ir ddl/certify-credential_status_transaction.sql 
-\ir ddl/certify-credential_template.sql 
+\ir ddl/certify-credential_status_transaction.sql
 \ir ddl/certify-key_alias.sql 
 \ir ddl/certify-key_policy_def.sql 
 \ir ddl/certify-key_store.sql 
