241cc0176b6774ef8ff0f8b0f8f194b09708ac13
Tejash JL <tejash.jl1020@gmail.com>
2025-11-14 14:50:30 +0530

    Set up foreign data wrapper and user mappings
    Added foreign data wrapper and user mappings for registry server. Created foreign table for verification counters and granted permissions to grafana_read.

--- db_scripts/mosip_certify/ddl.sql
+++ db_scripts/mosip_certify/ddl.sql
@@ -1,3 +1,37 @@
 \c :db
 
 \ir ddl/index.sql
+
+
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+CREATE SERVER registry_server
+    FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (host 'localhost', dbname 'registry', port '5432');
+
+CREATE USER MAPPING FOR CURRENT_USER
+    SERVER registry_server
+    OPTIONS (user 'postgres', password :'dbpass');
+
+CREATE USER MAPPING FOR grafana_read
+    SERVER registry_server
+    OPTIONS (user 'postgres', password :'dbpass');
+
+CREATE FOREIGN TABLE verification_counters (
+    id          integer NOT NULL,
+    created_at  timestamptz,
+    updated_at  timestamptz,
+    deleted_at  timestamptz,
+    type        text,
+    status      text,
+    date        timestamptz
+)
+SERVER registry_server
+OPTIONS (schema_name 'public', table_name 'verification_counters');
+
+
+GRANT USAGE ON SCHEMA certify TO grafana_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA certify TO grafana_read;
+
+GRANT USAGE ON SCHEMA public TO grafana_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafana_read;
