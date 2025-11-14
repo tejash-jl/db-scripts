c169b3ee286a52ea0e5c803977a1b41cde3351c8
Tejash JL <tejash.jl1020@gmail.com>
2025-11-14 12:46:14 +0530

    Update db.sql

--- db_scripts/mosip_certify/db.sql
+++ db_scripts/mosip_certify/db.sql
@@ -1,3 +1,4 @@
+
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -12,3 +13,24 @@ GRANT SELECT ON ALL TABLES IN SCHEMA certify TO grafana_read;
 
 ALTER DATABASE :db SET search_path TO certify,pg_catalog,public;
 
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+CREATE SERVER registry_server
+    FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (host 'localhost', dbname 'registry', port '5432');
+
+CREATE USER MAPPING FOR CURRENT_USER
+    SERVER registry_server
+    OPTIONS (user 'postgres', password :'dbpass');
+
+CREATE FOREIGN TABLE verification_counters (
+    id          integer NOT NULL,
+    created_at  timestamptz,
+    updated_at  timestamptz,
+    deleted_at  timestamptz,
+    type        text,
+    status      text,
+    date        timestamptz
+)
+SERVER registry_server
+OPTIONS (schema_name 'public', table_name 'verification_counters');
